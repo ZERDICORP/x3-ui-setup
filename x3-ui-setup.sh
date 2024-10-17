ca646624ccf133325d6d0d47a363d1c163f62615
Nikolay Anikin <56264511+ZERDICORP@users.noreply.github.com>
2024-10-17 21:42:04 +0300

    Update x3-ui-setup.sh

--- x3-ui-setup.sh
+++ x3-ui-setup.sh
@@ -3,4 +3,6 @@
 apt update && apt upgrade -y
 
 bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
-bash <(curl -Ls https://raw.githubusercontent.com/ZERDICORP/x3-ui-setup/main/x3-ui-autossl.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/cyb3rm4gus/3x-ui-auto_add_ssl/refs/heads/main/3x-ui-autossl.sh)
+
+x-ui restart
